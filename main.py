73cdf26df12332dc6fc8c9bb912498200ad9b1e7
KevinM <tianzeminkevin@gmail.com>
2019-12-04 09:03:01 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import kivy
+
+from kivy.config import Config
+# from kivy.core.window import Window
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+kivy.require("1.11.1")
+
+Config.set('graphics', 'resizable', False)
+Config.set('graphics', 'width', '1440')
+Config.set('graphics', 'height', '795')
+# Window.clearcolor = (1, 1, 1, 1)
+Builder.load_file('loginscreen.kv')
+
+
+class LoginScreenLayout(BoxLayout):
+    pass
+
+
+class KimberlyQuestApp(App):
+
+    def build(self):
+        return LoginScreenLayout()
+
+
+home = KimberlyQuestApp()
+home.run()
