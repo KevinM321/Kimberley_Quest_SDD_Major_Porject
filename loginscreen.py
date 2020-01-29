ac774d1fb75590bc0fd44dae0c57e1b81bcf7be0
KevinM <tianzeminkevin@gmail.com>
2020-01-29 20:03:34 +1100

    updated profile database

--- loginscreen.py
+++ loginscreen.py
@@ -1,15 +1,28 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from tinydb import TinyDB, Query
 
 
-
 class LoginScreenLayout(BoxLayout):
-    customer = ''
+    passenger = ''
 
     def __init__(self, **kwargs):
         super(LoginScreenLayout, self).__init__(**kwargs)
 
-    def login(self):
-        LoginScreenLayout.customer = customer_functions.Customer(self.usr_name_input.text_input.text,
-                                                                 self.psw_input.psw_input.text_input.text)
-        msg = LoginScreenLayout.customer.check()
+    # def login(self):
+    #     LoginScreenLayout.customer = passenger_functions.Passenger(self.usr_name_input.text_input.text,
+    #                                                                self.psw_input.psw_input.text_input.text)
+    #     msg = LoginScreenLayout.customer.check()
+
+
+class MyButton(Button):
+    pass
+
+
+class PasswordMask(MyButton):
+
+    # on press show/hide password
+    def on_release(self):
+        self.psw_input.password = not self.psw_input.password
+        self.text = "Hide" if self.text == "Show" else "Show"
+
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-import kivy
-import loginscreen
-
 from kivy.config import Config
+from loginscreen import *
+from homescreen import *
+from activityscreen import *
+from loginscreen import *
+
+import kivy
 # from kivy.core.window import Window
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager
 kivy.require("1.11.1")
 
 # open when finished
@@ -14,10 +18,14 @@ kivy.require("1.11.1")
 # Config.set('graphics', 'height', '790')
 # Window.clearcolor = (1, 1, 1, 1)
 Builder.load_file('loginscreen.kv')
+Builder.load_file('homescreen.kv')
+Builder.load_file('activityscreen.kv')
+Builder.load_file('locationscreen.kv')
+Builder.load_file('mealscreen.kv')
 
 
-class LoginScreenLayout(BoxLayout):
-    pass
+# class KQScreenManager(ScreenManager):
+#     pass
 
 
 class KimberlyQuestApp(App):
