6587cc8d74592be289edf2f774e3ca18d8f2a486
KevinM <tianzeminkevin@gmail.com>
2020-05-06 23:38:38 +1000

    added and changed so much

--- mealscreen.py
+++ mealscreen.py
@@ -15,9 +15,28 @@ class MealPanelItem(TabbedPanelItem):
 
     def __init__(self, **kwargs):
         super(MealPanelItem, self).__init__(**kwargs)
+        Clock.schedule_once(lambda dt: self.build_menu(), 0.5)
 
-    def on_release(self):
-        MealBox.body.generate_menu(self.text)
+    def build_menu(self):
+        menu_data = TinyDB('menu', indent=2).all()[0]
+        menu_meal = menu_data[self.text]
+        for each in menu_meal:
+            item = MenuItem()
+            item.name.text = each
+            item.desc.text = menu_meal[each]
+            self.meal_box.main_meal.add_widget(item)
+        menu_snack = menu_data['Snacks']
+        for each in menu_snack:
+            item = MenuItem()
+            item.name.text = each
+            item.desc.text = menu_snack[each]
+            self.meal_box.snacks.add_widget(item)
+        menu_drink = menu_data['Drinks']
+        for each in menu_drink:
+            item = MenuItem()
+            item.name.text = each
+            item.desc.text = menu_drink[each]
+            self.meal_box.drinks.add_widget(item)
 
 
 class MealTabs(TabbedPanel):
@@ -25,12 +44,18 @@ class MealTabs(TabbedPanel):
 
 
 class MealBox(BoxLayout):
+    pass
+    # def __init__(self, **kwargs):
+    #     super(MealBox, self).__init__(**kwargs)
+    #     MealBox.body = self
+    #
+    # def generate_menu(self, meal):
+    #     menu_data = TinyDB('menu', indent=2)
+    #     menu = menu_data.all()[0][meal]
+    #     self.main_meal.add_widget(Button(text='lol'))
+
+
+class MenuItem(BoxLayout):
 
     def __init__(self, **kwargs):
-        super(MealBox, self).__init__(**kwargs)
-        MealBox.body = self
-
-    def generate_menu(self, meal):
-        menu_data = TinyDB('menu', indent=2)
-        menu = menu_data.all()[0][meal]
-        self.main_meal.add_widget(Button(text='lol'))
+        super(MenuItem, self).__init__(**kwargs)
